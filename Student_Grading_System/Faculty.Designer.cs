8a5707bd7e17e3cbe87b1ee743c9a4c55646d084
f2019065185@umt.edu.pk <f2019065185@umt.edu.pk>
2022-06-07 12:21:54 +0500

    added forms

---
+++ Student_Grading_System/Faculty.Designer.cs
@@ -0,0 +1,147 @@
+﻿
+namespace Student_Grading_System
+{
+    partial class Faculty
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(372, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(368, 32);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Welcome TO Faculty Portal!";
+            // 
+            // listView1
+            // 
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(25, 135);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(634, 491);
+            this.listView1.TabIndex = 1;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(25, 86);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(193, 25);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Courses in progress:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(741, 156);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(321, 62);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Assesment";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(741, 254);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(321, 62);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Grading";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(741, 349);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(321, 62);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Announcement";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(741, 446);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(321, 62);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "Attendance";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(741, 544);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(321, 62);
+            this.button5.TabIndex = 7;
+            this.button5.Text = "Submit";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // Faculty
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1109, 715);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.label1);
+            this.Name = "Faculty";
+            this.Text = "Faculty";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+    }
+}
