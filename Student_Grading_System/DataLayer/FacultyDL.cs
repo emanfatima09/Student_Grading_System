60ff65d925726700dfb6f46ea3af4eba150af497
emanfatima09 <emanfatima2803@gmail.com>
2022-06-07 22:31:26 +0500

    hi

---
+++ Student_Grading_System/DataLayer/FacultyDL.cs
@@ -0,0 +1,37 @@
+﻿using Student_Grading_System.DataTransferObject;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Student_Grading_System.DataLayer
+{
+    class FacultyDL
+    {
+        DBConnect con;
+        public FacultyDL()
+        {
+            con = new DBConnect();
+        }
+        public LoginDTO VerifyUserFromDB(LoginDTO dto)
+        {
+            LoginDTO retDTO = new LoginDTO();
+            con.Con.Open();
+            string queryString = "SELECT * FROM MyUser WHERE UserName='" + dto.Username + "' AND Password='" + dto.Password + "';";
+            SqlCommand com = new SqlCommand(queryString, con.Con);
+            SqlDataReader reader = com.ExecuteReader();
+            while (reader.Read())
+            {
+                retDTO.Username = reader["UserName"].ToString();
+                retDTO.Password = reader["Password"].ToString();
+                retDTO.Role = reader["Role"].ToString();
+                con.Con.Close();
+                return retDTO;
+            }
+            con.Con.Close();
+            return null;
+        }
+    }
+}
